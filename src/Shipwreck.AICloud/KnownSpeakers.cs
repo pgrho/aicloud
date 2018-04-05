e471ab2c14b437582ae4922612d6acf5250336a8
pgrho <shipwreck@live.jp>
2018-04-05 19:18:51 +0900

    Defined known speaker_name

---
+++ src/Shipwreck.AICloud/KnownSpeakers.cs
@@ -0,0 +1,25 @@
+﻿namespace Shipwreck.AICloud
+{
+    public static class KnownSpeakers
+    {
+        public static readonly string Nozomi = "nozomi";
+        public static readonly string Sumire = "sumire";
+        public static readonly string Maki = "maki";
+        public static readonly string Kaho = "kaho";
+        public static readonly string Akari = "akari";
+        public static readonly string Nanako = "nanako";
+        public static readonly string Reina = "reina";
+        public static readonly string Seiji = "seiji";
+        public static readonly string Hiroshi = "hiroshi";
+        public static readonly string Osamu = "osamu";
+        public static readonly string Taichi = "taichi";
+        public static readonly string Koutarou = "koutarou";
+        public static readonly string Anzu = "anzu";
+        public static readonly string Yuuto = "yuuto";
+        public static readonly string Chihiro = "chihiro";
+        public static readonly string NozomiWithEmotion = "nozomi_emo";
+        public static readonly string MakiWithEmotion = "maki_emo";
+        public static readonly string ReinaWithEmotion = "reina_emo";
+        public static readonly string TaichiWithEmotion = "taichi_emo";
+    }
+}
