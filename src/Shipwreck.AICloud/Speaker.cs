0048d0db00d154f389ef7324340841f41173be26
pgrho <shipwreck@live.jp>
2018-04-08 09:57:02 +0900

    Added Speaker type

---
+++ src/Shipwreck.AICloud/Speaker.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Shipwreck.AICloud
+{
+    [DataContract]
+    public class Speaker
+    {
+        public Speaker() { }
+
+        internal Speaker(
+            string name, string displayName, bool isMale,
+            bool supportsJoy = false, bool supportsSadness = false, bool supportsAnger = false,
+            bool isWest = false)
+        {
+            _Name = name;
+            _DisplayName = displayName;
+            _IsMale = isMale;
+            _SupportsJoy = supportsJoy;
+            _SupportsSadness = supportsSadness;
+            _SupportsAnger = supportsAnger;
+            _IsWest = isWest;
+
+            IsReadOnly = true;
+        }
+
+        [IgnoreDataMember]
+        public bool IsReadOnly { get; }
+
+        private string _Name;
+        private string _DisplayName;
+        private bool _IsMale;
+        private bool _SupportsJoy;
+        private bool _SupportsSadness;
+        private bool _SupportsAnger;
+        private bool _IsWest;
+
+        [DataMember]
+        public string Name
+        {
+            get => _Name;
+            set => ThrowIfLocked()._Name = value;
+        }
+
+        [DataMember]
+        public string DisplayName
+        {
+            get => _DisplayName;
+            set => ThrowIfLocked()._DisplayName = value;
+        }
+
+        [DataMember]
+        public bool IsMale
+        {
+            get => _IsMale;
+            set => ThrowIfLocked()._IsMale = value;
+        }
+
+        [IgnoreDataMember]
+        public bool IsFemale
+        {
+            get => !IsMale;
+            set => ThrowIfLocked().IsMale = !value;
+        }
+
+        [DataMember]
+        public bool SupportsAnger
+        {
+            get => _SupportsAnger;
+            set => ThrowIfLocked()._SupportsAnger = value;
+        }
+
+        [DataMember]
+        public bool SupportsSadness
+        {
+            get => _SupportsSadness;
+            set => ThrowIfLocked()._SupportsSadness = value;
+        }
+
+        [DataMember]
+        public bool SupportsJoy
+        {
+            get => _SupportsJoy;
+            set => ThrowIfLocked()._SupportsJoy = value;
+        }
+
+        [DataMember]
+        public bool IsWest
+        {
+            get => _IsWest;
+            set => ThrowIfLocked()._IsWest = value;
+        }
+
+        private Speaker ThrowIfLocked()
+        {
+            if (IsReadOnly)
+            {
+                throw new InvalidOperationException();
+            }
+            return this;
+        }
+    }
+}
---
+++ src/Shipwreck.AICloud/Speakers.cs
@@ -0,0 +1,52 @@
+﻿namespace Shipwreck.AICloud
+{
+    public static class Speakers
+    {
+        public static Speaker[] GetKnownSpeakers()
+            => new[] {
+                Nozomi,
+                Sumire,
+                Maki,
+                Kaho,
+                Akari,
+                Nanako,
+                Reina,
+                Seiji,
+                Hiroshi,
+                Osamu,
+                Taichi,
+                Koutarou,
+                Anzu,
+                Yuuto,
+                Chihiro,
+                NozomiWithEmotion,
+                MakiWithEmotion,
+                ReinaWithEmotion,
+                TaichiWithEmotion,
+                MiyabiWest,
+                YamatoWest,
+            };
+
+        public static readonly Speaker Nozomi = new Speaker("nozomi", "のぞみ", false);
+        public static readonly Speaker Sumire = new Speaker("sumire", "すみれ", false);
+        public static readonly Speaker Maki = new Speaker("maki", "まき", false);
+        public static readonly Speaker Kaho = new Speaker("kaho", "かほ", false);
+        public static readonly Speaker Akari = new Speaker("akari", "あかり", false);
+        public static readonly Speaker Nanako = new Speaker("nanako", "ななこ", false);
+        public static readonly Speaker Reina = new Speaker("reina", "れいな", false);
+        public static readonly Speaker Seiji = new Speaker("seiji", "せいじ", true);
+        public static readonly Speaker Hiroshi = new Speaker("hiroshi", "ひろし", true);
+        public static readonly Speaker Osamu = new Speaker("osamu", "おさむ", true);
+        public static readonly Speaker Taichi = new Speaker("taichi", "たいち", true);
+        public static readonly Speaker Koutarou = new Speaker("koutarou", "こうたろう", true);
+        public static readonly Speaker Anzu = new Speaker("anzu", "あんず", false);
+        public static readonly Speaker Yuuto = new Speaker("yuuto", "ゆうと", true);
+        public static readonly Speaker Chihiro = new Speaker("chihiro", "ちひろ", false);
+        public static readonly Speaker NozomiWithEmotion = new Speaker("nozomi_emo", "のぞみ (感情対応)", false, supportsJoy: true, supportsSadness: true, supportsAnger: true);
+        public static readonly Speaker MakiWithEmotion = new Speaker("maki_emo", "まき (感情対応)", false, supportsJoy: true, supportsSadness: true, supportsAnger: true);
+        public static readonly Speaker ReinaWithEmotion = new Speaker("reina_emo", "れいな  (感情対応)", false, supportsJoy: true);
+        public static readonly Speaker TaichiWithEmotion = new Speaker("taichi_emo", " たいち (感情対応)", true, supportsJoy: true);
+        public static readonly Speaker MiyabiWest = new Speaker("miyabi_west", "みやび", false, isWest: true);
+        public static readonly Speaker YamatoWest = new Speaker("yamato_west", "やまと", true, isWest: true);
+    }
+}
